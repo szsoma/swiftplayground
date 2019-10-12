60beee325b8d09860e86772aedc7bbcfc53a6f04
Soma Szoboszlai <soma.szoboszlai>
2019-10-12 21:04:56 +0200

    apple tutorial list

---
+++ apple-tuts/BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Szoboszlai Soma on 2019. 10. 12..
+//  Copyright © 2019. Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        Text(landmark.name)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[1])
+    }
+}
